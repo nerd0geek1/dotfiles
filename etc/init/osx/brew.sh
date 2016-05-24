2870a79e4c630a388c8af7d17e12cb4ee21a47ae
Kohei Tabata <nerd0geek1@gmail.com>
2016-05-24 11:27:45 +0900

    export homebrew setup logic to etc/init/osx/brew.sh

---
+++ etc/init/osx/brew.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
+set -eu
+
+. "$DOTPATH"/etc/lib/vital.sh
+
+if has "brew"; then
+  log_pass "brew: already installed"
+  exit
+fi
+
+if ! has "ruby"; then
+  log_fail "error: require: ruby"
+  exit 1
+fi
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if has "brew"; then
+  brew doctor
+else
+  log_fail "error: brew: failed to install"
+  exit 1
+fi
+
+cd "$DOTPATH"/etc/init/assets/brew
+
+brew tap Homebrew/bundle
+brew bundle
+
+brew cleanup
+
+log_pass "brew: installed successfully"
