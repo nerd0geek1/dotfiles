f5f9e6bf348cd2fbf5b79faca75af84e71c498e4
Kohei Tabata <nerd0geek1@gmail.com>
2016-05-25 08:08:05 +0900

    add completion logs on gem/rbenv.sh

--- etc/init/osx/gem.sh
+++ etc/init/osx/gem.sh
@@ -3,8 +3,12 @@
 trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 
+. "$DOTPATH"/etc/lib/vital.sh
+
 gem install bundler
 
 cd "$DOTPATH"/etc/init/assets/gem
 
 bundle install --system
+
+log_pass "gem: installed successfully"
--- etc/init/osx/rbenv.sh
+++ etc/init/osx/rbenv.sh
@@ -3,6 +3,10 @@
 trap 'echo Error: $0:$LINENO stopped; exit 1' ERR INT
 set -eu
 
+. "$DOTPATH"/etc/lib/vital.sh
+
 rbenv install 2.2.3
 rbenv global 2.2.3
 rbenv rehash
+
+log_pass "rbenv: installed successfully"
