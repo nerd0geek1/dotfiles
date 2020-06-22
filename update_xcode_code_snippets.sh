94855862f10e8ff7a7af5283e4592991c29e9a60
Kohei Tabata <nerd0geek1@gmail.com>
2020-06-22 20:16:45 +0900

    improve update_xcode_code_snippets.sh

--- update_xcode_code_snippets.sh
+++ update_xcode_code_snippets.sh
@@ -1,1 +1,2 @@
+mkdir -p "$HOME/Library/Developer/Xcode/UserData/CodeSnippets"
 find ./xcode_code_snippet/*.codesnippet | xargs -I{} zsh -c "cp {} $HOME/Library/Developer/Xcode/UserData/CodeSnippets"
